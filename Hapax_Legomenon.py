61e5e03866a0f5b89f89ce3dae5e83ecae5f9657
Sergio Andrés Ramírez Céspedes <145515865+Serramirezce@users.noreply.github.com>
2026-03-02 18:40:14 +0000

    Code update

--- Hapax_Legomenon.py
+++ Hapax_Legomenon.py
@@ -1,4 +1,4 @@
-# Counts the number of times a word appears in a text(book)
+# Counts the number of times a word appears in a file
 def count_words(book):
     counts = {}
     for line in book:
@@ -8,10 +8,11 @@ def count_words(book):
             word = word.strip(",.!¡¿?:;[]'\"()")
             # Converts word to lowercase
             word = word.lower()
+            # Update the count for this word
             counts[word] = counts.get(word, 0) + 1
     return counts
 
-# Returns a list of the words that appear just one time
+# Returns a list of the words that appear just once in the text
 def hapax_legomenon(counts):
     hapax_list = []
     for word in counts:
@@ -19,10 +20,23 @@ def hapax_legomenon(counts):
             hapax_list.append(word)
     return hapax_list
 
-# Initialize the program, gets the user input (file name) and executes
+
+# Main program: Gets the user input (file name) and displays results
 if __name__ == "__main__":
+    # Prompts the input
     book_name = input("Enter the book name: ")
-    book = open(book_name, encoding='utf-8')
-    # print(count_words(book))
-    print("Hapax Legomenon words:", hapax_legomenon(count_words(book)))
-    book.close()
+
+    # Opens file:
+    with open(book_name, encoding='utf-8') as book:
+        word_counts = count_words(book)
+    
+    # Display list of Hapax Legomenon
+    print("Hapax Legomenon words:", hapax_legomenon(word_counts))
+
+    # Sorted list by frequency (ascending)
+    sorted_words = sorted(word_counts.items(), key=lambda item: item[1])
+
+    # Display the 10 most frequent words
+    print("\n10 most frequent words: \nWord: Times that word appears")
+    for word, freq in sorted_words[-10:]:
+        print(f"{word}: {freq}")
